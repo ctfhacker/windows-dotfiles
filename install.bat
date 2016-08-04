7b9d1b7db1a07b68686c3dad6fdf57a0aeb93028
thebarbershopper <cld251@gmail.com>
2016-08-04 15:42:51 -0600

    Don't need comments in install.bat anymore

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-rem ConEmuSetup.160619.exe
-rem msys2-x86_64-20160205.exe
-rem gvim74.exe
-rem reg import ConEmu.reg
+ConEmuSetup.160619.exe
+msys2-x86_64-20160205.exe
+gvim74.exe
+reg import ConEmu.reg
 FOR /F %%i in ('DIR /B /A:D dotfiles') DO mkdir C:\msys64\home\%username%\%%i && xcopy /Y /S /E /H dotfiles\%%i C:\msys64\home\%username%\%%i
 FOR /F %%i in ('DIR /B /A:HD dotfiles') DO mkdir C:\msys64\home\%username%\%%i && xcopy /Y /S /E /H dotfiles\%%i C:\msys64\home\%username%\%%i
 FOR /F %%i in ('DIR /B  dotfiles') DO xcopy /Y /H dotfiles\%%i C:\msys64\home\%username%
