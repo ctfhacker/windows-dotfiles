0bb5db1fe03b6d0b4eb3b3c647bacea126f1d5de
thebarbershopper <cld251@gmail.com>
2016-08-05 07:59:49 -0600

    Add prompt to ensure that pykd dlls are unblocked hose at a later date

--- install.bat
+++ install.bat
@@ -3,6 +3,8 @@ msys2-x86_64-20160205.exe
 gvim74.exe
 winsdk_web.exe
 reg import ConEmu.reg
+echo UNBLOCK THE PYKD DLLS... 
+Pause
 copy pykd-dlls\x64\pykd.dll "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\winext"
 copy pykd-dlls\x86\pykd.dll "C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\winext"
 FOR /F %%i in ('DIR /B /A:D dotfiles') DO mkdir C:\msys64\home\%username%\%%i && xcopy /Y /S /E /H dotfiles\%%i C:\msys64\home\%username%\%%i
