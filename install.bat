fa67a8219f97460ccc6b2e6c4281b7e2ad068a76
thebarbershopper <cld251@gmail.com>
2016-08-05 07:31:40 -0600

    Add install of pykd 2.0 ll have to manually pull those at a later date

--- install.bat
+++ install.bat
@@ -3,6 +3,8 @@ msys2-x86_64-20160205.exe
 gvim74.exe
 winsdk_web.exe
 reg import ConEmu.reg
+copy pykd-dlls\x64\pykd.dll "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\winext"
+copy pykd-dlls\x86\pykd.dll "C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\winext"
 FOR /F %%i in ('DIR /B /A:D dotfiles') DO mkdir C:\msys64\home\%username%\%%i && xcopy /Y /S /E /H dotfiles\%%i C:\msys64\home\%username%\%%i
 FOR /F %%i in ('DIR /B /A:HD dotfiles') DO mkdir C:\msys64\home\%username%\%%i && xcopy /Y /S /E /H dotfiles\%%i C:\msys64\home\%username%\%%i
 FOR /F %%i in ('DIR /B  dotfiles') DO xcopy /Y /H dotfiles\%%i C:\msys64\home\%username%
