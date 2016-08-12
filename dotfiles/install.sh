87cf8624ffbc86daa494c183f79e63be3f55432e
thebarbershopper <cld251@gmail.com>
2016-08-12 14:17:17 -0600

    Install syringe into user site-packages dir before/after building date

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -8,7 +8,15 @@ pacman --noconfirm -S mingw64/mingw-w64-x86_64-ag
 wget https://bootstrap.pypa.io/get-pip.py
 python2 get-pip.py
 python3 get-pip.py
-pip install ipython
+pip2 install ipython
 pip3 install ipython
 cp bin/* /usr/bin
 pip install virtualenvwrapper
+
+mkdir ~/workspace
+cd ~/workspace
+git clone https://github.com/arizvisa/syringe
+pip2 install six
+pip3 install six
+for file in $(ls syringe/lib); do ln -s $(pwd)/syringe/lib/$file `python2 -m site --user-site`; done
+for file in $(ls syringe/template); do ln -s $(pwd)/syringe/template/$file `python2 -m site --user-site`; done
